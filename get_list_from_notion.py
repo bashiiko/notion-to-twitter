059518ed697010eb9fe9a3487663c3a218f6c9af
naoko <bashiko.dev@gmail.com>
2021-04-12 23:04:13 +0900

    [update] タイトルリストをcsvをファイルで管理

--- get_list_from_notion.py
+++ get_list_from_notion.py
@@ -1,4 +1,5 @@
 from notion.client import NotionClient
+import csv
 
 # Obtain the `token_v2` value by inspecting your browser cookies on a logged-in (non-guest) session on Notion.so
 token = "your token"
@@ -13,9 +14,32 @@ print(page.title)
 # リストを取得
 cv = client.get_collection_view(url)
 
+
+def get_item_from_notion(row):
+    title = row.title
+    tag = row.tags
+    description = row.description
+    return title, tag, description
+
+def output_twitter(title, tag, description):
+    print(title)
+    print(description)
+    print(tag)
+
+
+with open('title_list.csv') as f:
+    reader = csv.reader(f)
+    title_list = [row[0] for row in reader]
+
+print(title_list)
+
 # リストの一覧を出力
 for row in cv.collection.get_rows():
-    print(row.title)
-    print(row.desctiption)
-    print(*row.tags)
-
+    title, tag, description = get_item_from_notion(row)
+    if title not in title_list:
+        output_twitter(title, tag, description)
+        with open('title_list.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([title])
+    else:
+        print('{0} is already added'.format(title))
