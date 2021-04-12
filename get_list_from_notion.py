59fca5099c5d56c56d9a6ca29a348b257b7971e5
naoko <bashiko.dev@gmail.com>
2021-04-12 23:13:47 +0900

    [update] output_twitterメソッドの体裁を整理

--- get_list_from_notion.py
+++ get_list_from_notion.py
@@ -2,14 +2,10 @@ from notion.client import NotionClient
 import csv
 
 # Obtain the `token_v2` value by inspecting your browser cookies on a logged-in (non-guest) session on Notion.so
-token = "your token"
 client = NotionClient(token_v2=token)
 
 # Replace this URL with the URL of the page you want to edit
-url = "your url"
 page = client.get_block(url)
-
-print(page.title)
 
 # リストを取得
 cv = client.get_collection_view(url)
@@ -21,25 +17,21 @@ def get_item_from_notion(row):
     description = row.description
     return title, tag, description
 
-def output_twitter(title, tag, description):
-    print(title)
-    print(description)
-    print(tag)
+def output_twitter(page, title, tag, description):
+    print('{0}に{1}を追加しました！'.format(page.title, title))
+    print('tag: {0}'.format('、'.join(tag)))
+    print('コメント: {0}'.format(description))
 
 
 with open('title_list.csv') as f:
     reader = csv.reader(f)
     title_list = [row[0] for row in reader]
 
-print(title_list)
-
 # リストの一覧を出力
 for row in cv.collection.get_rows():
     title, tag, description = get_item_from_notion(row)
     if title not in title_list:
-        output_twitter(title, tag, description)
+        output_twitter(page, title, tag, description)
         with open('title_list.csv', 'a') as f:
             writer = csv.writer(f)
             writer.writerow([title])
-    else:
-        print('{0} is already added'.format(title))
